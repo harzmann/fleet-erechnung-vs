105a097b7197b40c0cfd26b0b16bfac57b1d15ef
David Wellna <David.Wellna@ManagementStudio.com>
2024-05-22 16:56:40 +0100

    Initial commit

---
+++ EHWpfViews/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Stimulsoft.Report;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfCustomControlLibrary1
+{
+    public class MainViewModel : BindableBase
+    {
+        private EHFleetEntities _dbContext;
+
+        private StiReport _report;
+
+        public MainViewModel(EHFleetEntities dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public StiReport Report
+        {
+            get => _report;
+            set => SetProperty(ref _report, value);
+        }
+    }
+}
---
+++ EHWpfViews/WindowWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace WpfCustomControlLibrary1
+{
+    [ComVisible(true), Guid(ClassId)]
+    public class WindowWrapper
+    {
+        public const string ClassId = "3D853E7B-01DA-4944-8E65-5E36B501E889";
+
+        public void ShowWindow(string dbConnectionString)
+        {
+            try
+            {
+                var window = new ReportView();
+                window.DataContext = new MainViewModel(new EHFleetEntities(dbConnectionString));
+                window.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
