0e1f0756ca9f6df4516f85092e7afcdbbd0a7ca2
Jörg Harzmann <harzmann@eh-systemhaus.de>
2024-10-18 23:01:51 +0200

    Test ComOp

--- EHWinFormViews/WindowWrapper.vb
+++ EHWinFormViews/WindowWrapper.vb
@@ -5,12 +5,18 @@ Imports ehfleet_classlibrary
 Imports log4net.Config
 
 <Assembly: AssemblyKeyFile("..\\EHFleet.snk")>
+<ComVisible(True), Guid("3D853E7B-01DA-4944-8E65-5E36B501E889"), InterfaceType(ComInterfaceType.InterfaceIsDual)>
+Public Interface IShowWindow
+    <DispId(1)> Sub ShowWindow(connectionString As String, login As String)
+End Interface
 
-<ComVisible(True), Guid(WindowWrapper.ClassId)>
+<ComVisible(True), Guid("19E46122-C584-4DED-A53E-B23A2F0B3AA0"), ClassInterface(ClassInterfaceType.None)>
 Public Class WindowWrapper
-    Public Const ClassId As String = "3D853E7B-01DA-4944-8E65-5E36B501E889"
+    'Public Const ClassId As String = "3D853E7B-01DA-4944-8E65-5E36B501E889"
 
-    Sub ShowWindow(connectionString As String, login As String)
+    Implements IShowWindow
+
+    Public Sub ShowWindow(connectionString As String, login As String) Implements IShowWindow.ShowWindow
         ConfigureLogging(login)
         Dim form = New RechnungsUebersicht(New General.Database(connectionString))
         form.ShowDialog()
@@ -26,4 +32,5 @@ Public Class WindowWrapper
             XmlConfigurator.Configure(logConfigFile)
         End If
     End Sub
+
 End Class
